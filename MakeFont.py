0ec54946bdcd4e9410293811b53819244ea0da83
Frédéric Wang <fred.wang@free.fr>
2015-05-23 15:03:13 +0200

    Keep Unicode pieces and remove more characters.

--- MakeFont.py
+++ MakeFont.py
@@ -16,19 +16,99 @@ def binarySearchHasValue(aList, aValue):
     insertPosition = bisect.bisect_left(aList, aValue)
     return insertPosition < len(aList) and aList[insertPosition] == aValue
 
-def isBasicLatin(aGlyph):
-    return glyph.unicode >= 0 and glyph.unicode <= 0xFFF
+def isBasicLatin(aCodePoint):
+    return 0 <= aCodePoint and aCodePoint <= 0xFFF
 
-def isStretchable(aGlyph):
-    return (aGlyph.horizontalVariants is not None or
-            aGlyph.horizontalComponents is not None or
-            aGlyph.verticalVariants is not None or
-            aGlyph.verticalComponents is not None)
+def isGeneralPunctuation(aCodePoint):
+    return 0x2000 <= aCodePoint and aCodePoint <= 0x206F
+
+def isCombiningMarkForSymbols(aCodePoint):
+    return 0x20D0 <= aCodePoint and aCodePoint <= 0x20FF
+
+def isMiscellaneousTechnical(aCodePoint):
+    return 0x2300 <= aCodePoint and aCodePoint <= 0x23FF
+
+def isGeometricShape(aCodePoint):
+    return 0x25A0 <= aCodePoint and aCodePoint <= 0x25FF
+
+def isMiscelleanousSymbol(aCodePoint):
+    return 0x2600 <= aCodePoint and aCodePoint <= 0x26FF
+
+def isEnclosedAlphanumeric(aCodePoint):
+    return 0x2460 <= aCodePoint and aCodePoint <= 0x24FF
+
+def isDingBats(aCodePoint):
+    return 0x2700 <= aCodePoint and aCodePoint <= 0x27BF
+
+def isMiscelleanousMathSymbolB(aCodePoint):
+    return 0x2980 <= aCodePoint and aCodePoint <= 0x29FF
+
+def isMathOperatorSupplement(aCodePoint):
+    return 0x2A00 <= aCodePoint and aCodePoint <= 0x2AFF
+
+def isSupplementalSymbolAndArrow(aCodePoint):
+    return 0x2B00 <= aCodePoint and aCodePoint <= 0x2BFF
+
+def isHiragana(aCodePoint):
+    return 0x3040 <= aCodePoint <= 0x3097
+
+def isBoldMathVariant(aCodePoint):
+    return ((0x1D400 <= aCodePoint and aCodePoint <= 0x1D433) or
+            (0x1D6A8 <= aCodePoint and aCodePoint <= 0x1D6E1) or
+            (0x1D7CA <= aCodePoint and aCodePoint <= 0x1D7CB) or
+            (0x1D7CE <= aCodePoint and aCodePoint <= 0x1D7D7))
+
+def isBoldItalicMathVariant(aCodePoint):
+    return ((0x1D468 <= aCodePoint and aCodePoint <= 0x1D49B) or
+            (0x1D71C <= aCodePoint and aCodePoint <= 0x1D755))
+
+def isBoldScriptMathVariant(aCodePoint):
+    return 0x1D4D0 <= aCodePoint and aCodePoint <= 0x1D503
+
+def isBoldFrakturMathVariant(aCodePoint):
+    return 0x1D56C <= aCodePoint and aCodePoint <= 0x1D59F
+
+def isSansSerifMathVariant(aCodePoint):
+    return ((0x1D5A0 <= aCodePoint and aCodePoint <= 0x1D5D3) or
+            (0x1D7E2 <= aCodePoint and aCodePoint <= 0x1D7EB))
+
+def isBoldSansSerifMathVariant(aCodePoint):
+    return ((0x1D5D4 <= aCodePoint and aCodePoint <= 0x1D607) or
+            (0x1D756 <= aCodePoint and aCodePoint <= 0x1D78F) or
+            (0x1D7EC <= aCodePoint and aCodePoint <= 0x1D7F5))
+
+def isSansSerifItalicMathVariant(aCodePoint):
+    return 0x1D608 <= aCodePoint and aCodePoint <= 0x1D63B
+
+def isSansSerifBoldItalicMathVariant(aCodePoint):
+    return ((0x1D63C <= aCodePoint and aCodePoint <= 0x1D66F) or
+            (0x1D790 <= aCodePoint and aCodePoint <= 0x1D7C9))
+
+def isMonospaceMathVariant(aCodePoint):
+    return ((0x1D670 <= aCodePoint and aCodePoint <= 0x1D6A3) or
+            (0x1D7F6 <= aCodePoint and aCodePoint <= 0x1D7FF))
+
+def isStretchable(aFont, aCodePoint):
+    if aCodePoint not in aFont:
+        return False
+    glyph  = aFont[aCodePoint]
+    return (glyph.horizontalVariants is not None or
+            glyph.horizontalComponents is not None or
+            glyph.verticalVariants is not None or
+            glyph.verticalComponents is not None)
+
+def getSSTYList(aGlyph):
+    for table in aGlyph.getPosSub("*"):
+        if table[0].find("ssty") > 0:
+            return table[2:]
+    return None
+
+#def isNonItalicMathVariants()
 
 if __name__ == "__main__":
     # Check command line argument
-    if len(sys.argv) != 4:
-        print("usage: python %s input output charList" % sys.argv[0], file=sys.stderr)
+    if len(sys.argv) != 5:
+        print("usage: python %s input output charList metadata.xml" % sys.argv[0], file=sys.stderr)
         exit(1)
 
     # Open the input font.
@@ -85,7 +165,7 @@ if __name__ == "__main__":
     # Add WOFF information
     font.woffMajor = 1
     font.woffMinor = 1
-    with open("metadata.xml", "r") as metadataFile:
+    with open(sys.argv[3], "r") as metadataFile:
         font.woffMetadata = metadataFile.read()
 
     # Ensure that USE_TYPO_METRICS is set.
@@ -95,50 +175,83 @@ if __name__ == "__main__":
     font.os2_use_typo_metrics = True
 
     # Load the list of chars to preserve (MathML operators and mathclass)
-    kToPreserve = []
+    kBaseCharsToPreserve = []
     with open(sys.argv[2], "r") as charList:
         for line in charList:
             codePoints = line[1:len(line)-1].split("-");
-            if (len(codePoints) > 1):
-                # Let's exclude multiple characters
+            if len(codePoints) > 1:
+                # Exclude multiple characters.
                 continue
+
+            codePoint = int(codePoints[0], 16)
+
+            # Exclude more non-stretchable characters
+            if ((not isStretchable(font, codePoint)) and
+                (isBasicLatin(codePoint) or
+                 isGeneralPunctuation(codePoint) or
+                 isCombiningMarkForSymbols(codePoint) or
+                 isMiscellaneousTechnical(codePoint) or
+                 isGeometricShape(codePoint) or
+                 isMiscelleanousSymbol(codePoint) or
+                 isDingBats(codePoint) or
+                 isMiscelleanousMathSymbolB(codePoint) or
+                 isMathOperatorSupplement(codePoint) or
+                 isSupplementalSymbolAndArrow(codePoint) or
+                 isHiragana(codePoint) or
+                 isBoldMathVariant(codePoint) or
+                 isBoldItalicMathVariant(codePoint) or
+                 isBoldScriptMathVariant(codePoint) or
+                 isBoldFrakturMathVariant(codePoint) or
+                 isSansSerifMathVariant(codePoint) or
+                 isBoldSansSerifMathVariant(codePoint) or
+                 isSansSerifItalicMathVariant(codePoint) or
+                 isSansSerifBoldItalicMathVariant(codePoint) or
+                 isMonospaceMathVariant(codePoint))):
+                    continue
+
             # The list is probably already sorted, but just in case...
-            insortWithoutDuplicate(kToPreserve, int(codePoints[0], 16))
+            insortWithoutDuplicate(kBaseCharsToPreserve, codePoint)
 
-    # Remove the Unicode character outside kToPreserve as well as
-    # non-stretchable basic latin character.
-    font.selection.none()
-    for glyph in font.glyphs():
-        if glyph.unicode == -1:
+    # Determine the list of glyphs to preserve. It is made of glyphs used for
+    # characters in kBaseCharsToPreserve.
+    kGlyphsToPreserve = []
+    for u in kBaseCharsToPreserve:
+        if u not in font:
             continue
-        if ((not binarySearchHasValue(kToPreserve, glyph.unicode)) or
-            (isBasicLatin(glyph) and not isStretchable(glyph))):
-            font.selection.select(("more", None), glyph.glyphname)
-    font.clear()
+        glyph = font[u]
 
-    # Remove all non-Unicode characters that are not used for operator
-    # stretching
-    kToPreserve = []
-    for glyph in font.glyphs():
+        # Preserve the glyph for the base size.
+        insortWithoutDuplicate(kGlyphsToPreserve, glyph.glyphname)
+
+        # Preserve glyphs for size variants.
         if glyph.horizontalVariants is not None:
             for glyphname in glyph.horizontalVariants.split(" "):
-                insortWithoutDuplicate(kToPreserve, glyphname)
+                insortWithoutDuplicate(kGlyphsToPreserve, glyphname)
         if glyph.verticalVariants is not None:
             for glyphname in glyph.verticalVariants.split(" "):
-                insortWithoutDuplicate(kToPreserve, glyphname)
+                insortWithoutDuplicate(kGlyphsToPreserve, glyphname)
+
+        # Preserve glyphs used as components.
         if glyph.horizontalComponents is not None:
             for component in glyph.horizontalComponents:
-                insortWithoutDuplicate(kToPreserve, component[0])
+                insortWithoutDuplicate(kGlyphsToPreserve, component[0])
         if glyph.verticalComponents is not None:
             for component in glyph.verticalComponents:
-                insortWithoutDuplicate(kToPreserve, component[0])
+                insortWithoutDuplicate(kGlyphsToPreserve, component[0])
+
+        # Preserve glyphs used for ssty variants.
+        sstyList = getSSTYList(glyph)
+        if sstyList is not None:
+            for glyphname in sstyList:
+                insortWithoutDuplicate(kGlyphsToPreserve, glyphname)
+
+    # Now remove all the glyphs outside kGlyphsToPreserve.
     font.selection.none()
     for glyph in font.glyphs():
-        if (glyph.unicode == -1 and
-            (not binarySearchHasValue(kToPreserve, glyph.glyphname))):
+        if (not binarySearchHasValue(kGlyphsToPreserve, glyph.glyphname)):
             font.selection.select(("more", None), glyph.glyphname)
     font.clear()
 
     # Generate the subset and close the font.
-    font.generate(sys.argv[3])
+    font.generate(sys.argv[4])
     font.close()
--- README.md
+++ README.md
@@ -9,3 +9,10 @@ of the STIX fonts intended to be bundled into Mozilla applications and systems.
 * MOZTIXMath-Regular.otf: 288.5 kb (288464 bytes)
 * MOZTIXMath-Regular.woff: 200.1kb (200068 bytes)
 * MOZTIXMath-Regular.woff2: 165.6kb (165632 bytes)
+
+23/05/2015:
+
+* STIXMath-Regular.otf: 476.9kb (476872 bytes)
+* MOZTIXMath-Regular.otf: 156kb (155992 bytes)
+* MOZTIXMath-Regular.woff: 102.9kb (102944 bytes)
+* MOZTIXMath-Regular.woff2: 85.2kb (85192 bytes)
